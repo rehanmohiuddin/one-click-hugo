7355d4d728e468b6587d496f2bf9d252fd7f47ea
Rehan Mohiuddin <rehan.4942@gmail.com>
2020-08-09 15:19:48 +0530

    update6

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -14,6 +14,7 @@ export default class PostPreview extends React.Component {
       </div>
       <div className="cms mw6">
         <p>{ entry.getIn(["data", "description"]) }</p>
+        <p>{ entry.getIn(["data", "status"]) }</p>
         { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
         { widgetFor("body") }
       </div>
